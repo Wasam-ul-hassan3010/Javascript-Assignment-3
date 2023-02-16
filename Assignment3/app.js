67dbd72bb9173785a35d9793c82e42992588e917
Wasam-ul-hassan3010 <124611992+Wasam-ul-hassan3010@users.noreply.github.com>
2023-02-16 19:55:37 +0500

    Add files via upload

---
+++ Assignment3/app.js
@@ -0,0 +1,161 @@
+// CHAPTER 05 MATH EXPRESSIONS //
+
+// Question1*
+// var num1 = 2;
+// var num2 = 4;
+// var total = (num1 + num2)
+// document.write(total)
+
+
+// Question 02*
+// var num1 = 8;
+// var num2 = 4;
+// var total = (num1 - num2)
+// document.write(total)
+
+// var num1 = 2;
+// var num2 = 4;
+// var total = (num1 * num2)
+// document.write(total)
+
+// var num1 = 10;
+// var num2 = 2;
+// var total = (num1 / num2)
+// document.write(total)
+
+// var num1 = 10;
+// var num2 = 2;
+// var total = num1 % num2;
+// document.write(total)
+
+
+// Question 03*
+// var num;
+// document.write("Value after variable declaration is:" + num)
+// num = 5
+// document.write("Initial value:" + num)
+// num = ++num
+// document.write("Value after increment is:" + num)
+// num = num + 7
+// document.write("Value after addition is:" + num)
+// num = --num
+// document.write("Value after decrement is:" + num)
+// num = num % 3
+// document.write("The remainder is :" + num)
+
+
+// Question 04*
+// var ticketPrice = 600
+// var totalTickets = ticketPrice * 5
+// document.write("Total cost to but 5 tickets to a movie is" + " " + totalTickets + " " + "PKR")  
+
+
+
+// Question 05*
+// document.write("<h2>Table of 3</h2>")
+//     var aa = 1
+//     var ab = 2
+//     var ac = 3
+//     var ad = 4
+//     var ae = 5
+//     var af = 6
+//     var ag = 7
+//     var ah = 8
+//     var ai = 9
+//     var aj = 10
+
+//     var ta = ac * aa  
+//     document.write("3x1=" + ta + "<br>")
+
+//     var tb = ac * ab
+//     document.write("3x2=" + tb + "<br>")
+
+//     var tc = ac * ac 
+//     document.write("3x3=" + tc + "<br>")
+
+//     var td = ac * ad
+//     document.write("3x4=" + td + "<br>")
+
+//     var te =  ac * ae
+//     document.write("3x5=" + te + "<br>")
+
+//     var tf = ac * af
+//     document.write("3x6=" + tf + "<br>")
+
+//     var tg = ac * ag
+//     document.write("3x7=" + tg + "<br>")
+
+//     var th = ac * ah
+//     document.write("3x8=" + th + "<br>")
+    
+//     var ti = ac * ai
+//     document.write("3x9=" + ti + "<br>")
+
+//     var tj = ac * aj
+//     document.write("3x10=" + tj + "<br>")
+
+
+// Question 06*
+// var tem_c = 16;
+// var f = (tem_c * 9/5)+32
+// document.write(tem_c + "<sup>0</sup> C is" + " " + f + " <sup>0</sup> F <br>");
+// var tem_f = 60;
+// var c = (tem_f - 32) * 5/9;
+// document.write(tem_f + "<sup>0</sup> F is" + " " + c +  " <sup>0</sup> C");
+
+
+// Question 07*
+// var item1 = 650;
+// var quantity = 3;
+// var totalItem1 = item1 * quantity
+// var item2 = 100;
+// var quantity = 7;
+// var totalItem2 = item2 * quantity
+// var shppingCharges = 100
+
+// var totalAmmount = totalItem1 + totalItem2 + shppingCharges
+
+// document.write("Total cost of your order is" + totalAmmount)
+
+// Question 08*
+// var totalMarks = 980;
+// var obtainedMarks = 804;
+// var total = totalMarks / obtainedMarks;
+// document.write("Percentage" + " " + total + "%")
+
+// Question 09*
+// var dollars = 104.80;
+// var riyals = 28;
+// var total = dollars * 10 + riyals * 25
+// document.write("Total currency in PKR:" + total)
+
+
+// Question 10*
+// var num = 5;
+// var calculation = (num + 5) * 10 / 2;
+// document.write(calculation)
+
+// Question 11*
+// var currenctYear = 2022
+// var birthYear = 2002
+// var age = currenctYear - birthYear
+// document.write(age)
+
+//Question 12*
+// var radius = 20
+// var pi = 3.142
+// var circumfurence = 2 * pi * radius;
+// document.write("The circumference is:" + circumfurence)
+// var area = pi * radius * radius;
+// document.write(area)
+
+
+// Question 13*
+// var favSnacks = "Lays"
+// var age = 20
+// var maxAge = 60
+// var perDay = 5
+// var total = (60 * 20) / 5
+// document.write("You will need " + total + " to last you until the ripe old age of " + maxAge)
+
+
---
+++ Assignment3/main.js
@@ -0,0 +1,38 @@
+// Question 14*
+// var a = 10
+// a = ++a
+// a = a++
+// a = --a
+// a = a--
+// document.write(a)
+
+// Question 15*
+// var a = 2 
+// var b = 1
+// a = --a;
+// document.write("A is " + a)
+// b = --b + ++b + b--;
+// document.write("B is " + b)
+// var result = a + b;
+// document.write("result is" + result)
+
+
+// Question 16*
+// prompt("Enter your name")
+
+
+// Question 17*
+// var number = prompt("Enter your number" , "5")
+// var result = number * 5
+// alert(result)
+
+// Question 18*
+// var subject1 = prompt("First subject");
+// var subject2 = prompt("Second subject");
+// var subject3 = prompt("Third subject");
+// var total = 100
+// var obtMarksSub1 = prompt("Give your first sub marks")
+// var obtMarksSub2 = prompt("Give your first sub marks")
+// var obtMarksSub3 = prompt("Give your first sub marks")
+// document.write("<table> <tr> <th>Subject total marks</th> <th>Obtained marks</th> <th>Percentage</th> </tr> </table>"
+// document.write("")
